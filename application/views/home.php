064576867de2bc707e25da04fe46ad1030bd82d9
Gazel <gazel@ngoprekweb.com>
2024-11-21 16:11:33 +0700

    style(wip): home page

---
+++ application/views/home.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Match-Cut Barber</title>
+
+	<!-- FONTS  -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Barlow:wght@800;900&family=Work+Sans:ital,wght@0,500;1,500&display=swap" rel="stylesheet">
+
+	<!-- STYLES -->
+	<link rel='stylesheet' href='<?= base_url() ?>assets/css/header.css'>
+</head>
+
+<body>
+
+	<div class="header">
+		<div class="header-text">
+			<h1>THE ULTIMATE CONVENIENCE</h1>
+			<H1>FOR BUSY PEOPLE</H1>
+			<p>Experience the Convenience of In-Home Barber Services</p>
+		</div>
+		<div class="header-button">
+			<button>BOOK AN APPOINTMENT</button>
+			<button>BROWSE SERVICES</button>
+		</div>
+	</div>
+</body>
+
+</html>
